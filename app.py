19e1929b40257b7769fea88b8ee6484c6d2a2662
Anujabele <beleanuja6@gmail.com>
2025-06-18 01:31:49 +0530

    added app.py with flask and extension and flask setup

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
 
-@app.route('/')
-def home():
-    return "Welcome to Krushi Kendra Inventory System"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-if __name__ == '__main__':
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from models import User, Product  # You’ll create these in models.py
+
+if __name__ == "__main__":
     app.run(debug=True)
